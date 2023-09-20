c379f7c27affa1727a42d9f8416016a981f18135
karachid <r.kabbab@gmail.com>
2023-09-20 16:45:35 -0700

    added pop opcode among the list

--- LineParser.c
+++ LineParser.c
@@ -94,6 +94,7 @@ int parse_execute_line(char *line, stack_t **top, unsigned int ln, FILE *fp)
 	instruction_t instructions[] = {
 		{"pall", print_all},
 		{"pint", print_top},
+		{"pop", pop_top},
 		{NULL, NULL}
 	};
 
